d2eb45ff71419c86c348fd8add1a464b131a7431
GurpartapSS <gurpartapsingh.sarkaria@gmail.com>
2022-03-07 16:34:24 -0500

    Finishing schedular algorithm

--- src/sched.c
+++ src/sched.c
@@ -19,13 +19,46 @@ void schedule_tail(void) {
     preempt_enable();
 }
 
-void schedule(void) {
+void _schedule(void) {
     preempt_disable();
     int next, c;
     task_struct_t *p;
     while(1) {
         c = -1;
         next = 0;
-
+        for (int i = 0; i < NR_TASKS; i++) {
+            p = task[i];
+            if(p && p->state == TASK_RUNNING && p->counter > c) {
+                c = p->counter;
+                next = i;
+            }
+        }
+        if(c) {
+            break;
+        }
+        for (int i = 0; i < NR_TASKS; i++) {
+            p = task[i];
+            if(p) {
+                p->counter = (p->counter >> 1) + p->priority;
+            }
+        }
     }
+
+    switch_to(task[next]);
+    preempt_enable();
+}
+
+void schedule(void) {       
+    current->counter = 0;
+    _schedule();
+}
+
+void switch_to(task_struct_t *next) {
+    if(current == next) {
+        return;
+    }
+    task_struct_t *prev = current;
+    current = next;
+    cpu_switch_to(prev, next);
+
 }
